9b9203ed0921e9c52d96579ad08212f95a607af1
Alexei Boev <alex@maximumweb.ru>
2012-10-12 23:40:41 +0400

    almost finalized combined rss generation mechanism

--- viewfeed.php
+++ viewfeed.php
@@ -16,6 +16,37 @@
 	while( $row = mysql_fetch_array($sql, MYSQL_ASSOC) ) {
 		$feeds[] = $row['feed_url'];
 	}
+	
+	$i = 0; //just an array counter
+	$total_links = 0; //total quantity of links in all incoming RSS feeds
+	$max = 20; //maximum amount of links in export RSS feed (should be get from DB later)
+	
+	foreach( $feeds as $key=>$value ) {
+		$xml = simplexml_load_file( $value );
+		$j = 0;
+		foreach( $xml->channel->item as $item ) {
+			$_RSS_PREPARE[$key][$j]['link'] = trim($item->link);
+			$_RSS_PREPARE[$key][$j]['image'] = $item->enclosure["url"];
+			$_RSS_PREPARE[$key][$j]['imagelength'] = $item->enclosure["length"];
+			$_RSS_PREPARE[$key][$j]['imagetype'] = $item->enclosure["type"];
+			$_RSS_PREPARE[$key][$j]['title'] = $item->title;
+			$_RSS_PREPARE[$key][$j]['description'] = $item->description;
+			$_RSS_PREPARE[$key][$j]['pubdate'] = $item->pubDate;
+			$j++;
+			$total_links++;
+		}
+		$i++;
+	}
+	
+	$total_input_RSS = $i;
+	
+	for( $i = 0; $i < $total_links; $i++ ) {
+		for ( $j = 0; $j < $total_input_RSS; $j++ ) {
+			if( !empty( $_RSS_PREPARE[$j][$i] ) ) {
+				$_RSS_EXPORT[] = $_RSS_PREPARE[$j][$i];				
+			}
+		}
+	}
 
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>";
 	echo "<rss version=\"2.0\">";
@@ -24,36 +55,19 @@
 	echo "<link>http://ru.msn.com/</link>";
 	echo "<description></description>";
 	
-	$i = 0;
-	$max = 4;
-	
-	foreach( $feeds as $key=>$value ) {
-		$xml = simplexml_load_file( $value );
-		foreach( $xml->channel->item as $item ) {
-			$_SQL['link'] = trim($item->link);
-			$_SQL['image'] = $item->enclosure["url"];
-			$_SQL['imagelength'] = $item->enclosure["length"];
-			$_SQL['imagetype'] = $item->enclosure["type"];
-			$_SQL['title'] = $item->title;
-			$_SQL['description'] = $item->description;
-			$_SQL['pubdate'] = $item->pubDate;
-			echo "<item>";
-			echo "<title>".$_SQL['title']."</title>";
-			echo "<link>".$_SQL['link']."</link>";
-			echo "<guid isPermaLink=\"true\">".$_SQL['link']."</guid>";
-			echo "<description>".$_SQL['description']."</description>";
-			echo "<enclosure length=\"".$_SQL['imagelength']."\" type=\"".$_SQL['imagetype']."\"></enclosure>";
-			echo "<pubDate>".$_SQL['pubdate']."</pubDate>";
-			echo "</item>";
-		}
-		if( $i == $max-1 ) {
-			break;
-		}
-		$i++;
-	}
+	foreach( $_RSS_EXPORT as $key => $value ) {
+		echo "<item>";
+		echo "<title>".$value['title']."</title>";
+		echo "<link>".$value['link']."</link>";
+		echo "<guid isPermaLink=\"true\">".$value['link']."</guid>";
+		echo "<description>".$value['description']."</description>";
+		echo "<enclosure length=\"".$value['imagelength']."\" type=\"".$value['imagetype']."\"></enclosure>";
+		echo "<pubDate>".$value['pubdate']."</pubDate>";
+		echo "</item>";		
+	}	
 	
 	echo "</channel>";
 	echo "</rss>";
-
+		
 	mysql_close();
 ?>
