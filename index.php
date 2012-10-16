8195b4c20aaef1f3fadb19d60c1188b19f88b8f7
Alexei Boev <alex@maximumweb.ru>
2012-10-16 11:11:43 +0400

    minor interface changes

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 		echo 
 		"<li class=\"dropdown\">
 			<a href=\"/providers.php?providerid=".$row['provider_id']."\">
-				".$row['provider_name']."</a> <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\"> <b class=\"caret\"></b></a>
-			</a>
+				".$row['provider_name']."
+			</a> <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\"><i class=\"icon-cog\"></i></a>
 			<ul class=\"dropdown-menu\">
 				<li><a href=\"#test_modal\" data-toggle=\"modal\">Удалить</a></li>
 				<li><a href=\"#\">Переименовать</a></li>
@@ -22,9 +22,20 @@
 	
 	$sql = mysql_query("SELECT * FROM `export_feeds`");
 	echo "<h2>Список выходных потоков</h2>";
+	echo "<ul>";
 	while( $row = mysql_fetch_array($sql, MYSQL_ASSOC)) {
-		echo "<a href=\"/viewfeed.php?feedid=".$row['exportfeed_id']."\">".$row['exportfeed_name']."</a><br/>";
+		echo "
+		<li class=\"dropdown\">
+			<a href=\"/viewfeed.php?feedid=".$row['exportfeed_id']."\">
+				".$row['exportfeed_name']."</a> <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\"><i class=\"icon-cog\"></i></a>
+			<ul class=\"dropdown-menu\">
+				<li><a href=\"#text_view\" /><i class=\"icon-list\"></i> Посмотреть как таблица</a></li>
+				<li><a href=\"#text_view\" /><i class=\"icon-file\"></i> Посмотреть как XML</a></li>				
+			</ul>
+		</li>
+		";
 	}
+	echo "</ul>";
 	
 	mysql_close();
 ?>
